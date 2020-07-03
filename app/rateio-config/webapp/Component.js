4ee7c81c306780675de3a5381f00602bae5ac5e0
Sebastian Goldberg <sebas.goldberg@gmail.com>
2020-07-03 23:40:50 +0000

    - Adição de pagina FLP de teste. - Definição basica de rotas. - Adição do app ui5 de configuração de rateios baseado no Fiori Elements.

---
+++ app/rateio-config/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("qintess.rateio.rateio-config.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
